00cd3f7c72f3f7ca21a8631ea26fd83bb7ea1aba
nfgrawker <nfgrawker@gmail.com>
2018-10-01 17:16:33 -0500

    Creating framework for saving progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 var emotionArray = ["Angry","Sad","Mad","Embarrased","Overjoyed","Happy","Anxious","Livid"]
-
-
+var gifobject = {
+  
+}
 emotionArray.forEach(function(emotion){
   var button = $("<button>")
-  button.addClass("btn btn-info original ml-1 mt-2 gifbtn")
+  button.addClass("btn btn-info original m-2 gifbtn")
   button.attr("searchterm",emotion)
   button.text(emotion)
   $(".buttons").append(button)
@@ -13,17 +14,17 @@ emotionArray.forEach(function(emotion){
 $(".mknew").on("click",function(){
   var buttonname = $("#search").val().trim()
   var button = $("<button>")
-  button.addClass("btn btn-info new ml-1 mt-2 gifbtn")
+  button.addClass("btn btn-info new m-2 gifbtn")
   button.attr("searchterm",buttonname)
   button.text(buttonname)
   $(".buttons").append(button)
 })
 $(document).on('click','.gifbtn',function(){
-  $(".pictures").empty()
+  // $(".pictures").empty()
   var emotion = $(this).attr("searchterm").trim()
   apikey = "M9iZOfoKJVRIUjlHZSeifrswb8vjiDVb"
   $.ajax({
-    url : "https://api.giphy.com/v1/gifs/search?q="+emotion+"&api_key="+apikey,
+    url : "https://api.giphy.com/v1/gifs/search?q="+emotion+"&api_key="+apikey+"&limit=100",
     method : "GET"
   }).then(function(response){
     for (var i=0;i<10;i++){
@@ -36,40 +37,25 @@ $(document).on('click','.gifbtn',function(){
       image.addClass("m-2 changeState")
       newDiv.append(image)
       newDiv.append("<p class = 'bg-light'>Rating: "+response.data[i].rating+"</p>")
-      $(".pictures").append(newDiv)
+      $(".pictures").prepend(newDiv)
     }
-    })
-    $(".pictures").on('click','.changeState',function(){
-          var state = $(this).attr("data-state")
-          var animate = $(this).attr("data-animate")
-          var still = $(this).attr("data-still")
-          if (state == "still") {
-          $(this).attr("src",animate)
-          $(this).attr("data-state","animate")
-          }
-          else{
-          $(this).attr("src",still)
-          $(this).attr("data-state","still")
-        }
-        })
-
+})
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(".pictures").on('click','.changeState',function(){
+    var state = $(this).attr("data-state")
+    var animate = $(this).attr("data-animate")
+    var still = $(this).attr("data-still")
+    if (state == "still") {
+    $(this).attr("src",animate)
+    $(this).attr("data-state","animate")
+    }
+    else{
+    $(this).attr("src",still)
+    $(this).attr("data-state","still")
+  }
+  })
+  $(".clear").on("click",function(){
+    $(".pictures").empty()
+})
 })
